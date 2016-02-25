1d35e6f9fa6dff60d09a1b8a4ddcf6c420770745
Tyler Deans <tyler@geekingdoing.com>
2016-02-25 15:34:44 -0500

    set up plan for filter model

--- app/scripts/filtermodel.js
+++ app/scripts/filtermodel.js
@@ -11,7 +11,28 @@ function FilterModel(_simModel) {
   this.simModel = _simModel;
 }
 
+/*
+ * There should be a helper method that generates a list with a random number of elements
+ * For lists of numbers and strings
+ * A method that creates a number list which calls the number of list elements generator method
+ * Then appends a number (generated) randomly to the list (for loop until the end of the list)
+ * 
+ * The string list generator is similar to the number list generator
+ * Except that the elements come from a large list of strings
+ * The string from that list is picked randomly (index is randomly chosen)
+ * Then the string is appended to the list
+ *	
+ * A method to determine the question type (number or string)
+ * returns a string (number or string)
+ *
+*/
+
 FilterModel.prototype.randomFilterExpression = function() {
+	/*
+	* Based on the question type create the filter expression string
+	* The answer should be a list with zero or more elements (zero elements look like [])
+	* The answer is saved in a local varible and is returned
+	*/
 	
 }
 
